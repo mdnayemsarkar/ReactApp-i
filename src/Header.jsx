0455f7e3c1f20126d80ce8adce9f3b5d9f73e4f9
Nayeem <nayeem3334@gmail.com>
2023-07-10 22:42:17 +0600

    update

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,27 +1,156 @@
-import React from "react";
-import { Navbar } from "flowbite-react";
-import hdrlgo from "../src/image/Believe in Yourself.png";
-import { NavLink } from "react-router-dom";
+// import React from "react";
+// import { Navbar } from "flowbite-react";
+// import hdrlgo from "../src/image/Believe in Yourself.png";
+// import { NavLink } from "react-router-dom";
 
-function Header() {
+// function Header() {
+//   return (
+//     <>
+//       <Navbar fluid rounded className="bg-black">
+//         <Navbar.Brand href="/navbars">
+//           <img alt="Flowbite Logo" className="mr-3 h-6 sm:h-9" src={hdrlgo} />
+//         </Navbar.Brand>
+//         <Navbar.Toggle />
+//         <Navbar.Collapse>
+//           <Navbar.Link active href="/home">
+//             Home
+//           </Navbar.Link>
+//           <Navbar.Link href="/about">About</Navbar.Link>
+//           <Navbar.Link href="/services">Services</Navbar.Link>
+//           <Navbar.Link href="/contact">Contact</Navbar.Link>
+//         </Navbar.Collapse>
+//       </Navbar>
+//     </>
+//   );
+// }
+
+// export default Header;
+
+import React, { useState } from "react";
+import img from "../src/image/Nayeem.png";
+import { Navbar } from "flowbite-react";
+
+const Header = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
   return (
     <>
-      <Navbar fluid rounded className="bg-black">
-        <Navbar.Brand href="/navbars">
-          <img alt="Flowbite Logo" className="mr-3 h-6 sm:h-9" src={hdrlgo} />
-        </Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse>
-          <Navbar.Link active href="/home">
-            Home
-          </Navbar.Link>
-          <Navbar.Link href="/about">About</Navbar.Link>
-          <Navbar.Link href="/services">Services</Navbar.Link>
-          <Navbar.Link href="/contact">Contact</Navbar.Link>
-        </Navbar.Collapse>
-      </Navbar>
+      {/* <Navbar></Navbar> */}
+      <nav className="bg-white border-gray-200 dark:bg-gray-900">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <a href="https://flowbite.com/" className="flex items-center">
+            <img
+              src="https://flowbite.com/docs/images/logo.svg"
+              className="h-8 mr-3"
+              alt="Flowbite Logo"
+            />
+            <span className="self-center text-2xl font-semibold font-inconsolata whitespace-nowrap dark:text-white">
+              Nayem Sarkar
+            </span>
+          </a>
+          <div className="flex items-center md:order-2">
+            <button
+              type="button"
+              className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+              id="user-menu-button"
+              aria-expanded="false"
+              data-dropdown-toggle="user-dropdown"
+              data-dropdown-placement="bottom"
+            >
+              <span className="sr-only">Open user menu</span>
+              <img
+                className="w-8 h-8 rounded-full"
+                src={img}
+                alt="user photo"
+              />
+            </button>
+            {/* User dropdown menu */}
+            <div
+              className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
+              id="user-dropdown"
+            >
+              {/* Dropdown content */}
+            </div>
+            {/* Hamburger menu */}
+            <button
+              type="button"
+              className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              onClick={handleMenuToggle}
+              aria-controls="mobile-menu"
+              aria-expanded={isMenuOpen}
+            >
+              <span className="sr-only">Open main menu</span>
+              <svg
+                className="w-6 h-6"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
+          </div>
+          {/* Mobile menu */}
+          <div
+            className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${
+              isMenuOpen ? "block" : "hidden"
+            }`}
+            id="mobile-menu"
+          >
+            <ul className="flex flex-col font-medium font-Poppins p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <li>
+                <Navbar.Link
+                  href="/"
+                  className="block py-2 pl-3 pr-4 text-blue-400 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  Home
+                </Navbar.Link>
+              </li>
+              <li>
+                <Navbar.Link
+                  href="/about"
+                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  About
+                </Navbar.Link>
+              </li>
+              <li>
+                <Navbar.Link
+                  href="/services"
+                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  Services
+                </Navbar.Link>
+              </li>
+              <li>
+                <Navbar.Link
+                  href="/pricing"
+                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  Pricing
+                </Navbar.Link>
+              </li>
+              <li>
+                <Navbar.Link
+                  href="/contact"
+                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  Contact
+                </Navbar.Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </>
   );
-}
+};
 
 export default Header;
